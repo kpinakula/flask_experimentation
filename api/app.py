311f9c37d98f01dfe92875f9680fb07340a2978a
kpinakula <7438240+kpinakula@users.noreply.github.com>
2023-03-14 15:55:19 +0000

    Create "/hello" route and test response object contains garden welcome message (in json format)
    Co-authored-by: Barbora Kaminskas <babskami@users.noreply.github.com>
    Co-authored-by: Josephine <Joseaie@users.noreply.github.com>
    Co-authored-by: SaraAyubKhanMajles <SaraAyubKhanMajles@users.noreply.github.com>
    Co-authored-by: Kyle <kjdchapman@users.noreply.github.com>

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/hello")
+def get_welcome_message():
+    return jsonify({"Hello": "Welcome to the garden"})
--- tests/app_test.py
+++ tests/app_test.py
@@ -1,7 +1,15 @@
 from api.app import app
+import json
 
 
 def test_root_returns_hello_world():
     client = app.test_client()
     response = client.get("/")
     assert response.data == b"<p>Hello, World!</p>"
+
+
+def test_hello_route_returns_welcome_message():
+    client = app.test_client()
+    response = client.get("/hello")
+    json_response = json.loads(response.data)
+    assert json_response["Hello"] == "Welcome to the garden"
